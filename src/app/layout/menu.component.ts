ab56a15a8a166646eebbcbc1d47e3bbc9258b2e4
Jeongsu Lee <lint@Jeongsus-MacBook-Pro.local>
2019-01-28 21:25:35 +0900

    Get feeds when search-input is changed

--- src/app/layout/menu.component.ts
+++ src/app/layout/menu.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  selector: "app-menu",
+  templateUrl: "./menu.component.html",
+  styleUrls: ["./menu.component.css"]
 })
 export class MenuComponent implements OnInit {
   @Output() updateKeyword = new EventEmitter<any>();
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSubmit(form: NgForm) {
-    const search_keyword = form.value['search-input'];
+    const search_keyword = form.value["search-input"];
     // this.updateKeyword.emit(search_keyword);
-      this.router.navigate(['/feeds', search_keyword]);
+    this.getCommentList(search_keyword);
     // this.feedComponent.getCommentList(search_keyword);
+  }
+
+  getCommentList(search_keyword: string) {
+    this.router.navigate(["/feeds", search_keyword]);
+  }
+  onSearchChange(search_keyword: string) {
+    if (search_keyword.length >= 2) {
+      this.getCommentList(search_keyword);
+    }
   }
 }
