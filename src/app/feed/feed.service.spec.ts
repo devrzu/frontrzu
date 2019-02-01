b9ed827830f09c4eafbb252207bab7b8884dc9dd
Eunchong Lee <gr4ce@korea.ac.kr>
2019-02-01 16:30:31 +0900

    fix test spec

--- src/app/feed/feed.service.spec.ts
+++ src/app/feed/feed.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
 import { FeedService } from './feed.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HelperService } from '../services';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
 
 describe('FeedService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    return TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [HelperService, SlimLoadingBarService]
+    });
+  });
 
   it('should be created', () => {
     const service: FeedService = TestBed.get(FeedService);
